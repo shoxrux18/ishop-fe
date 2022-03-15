2ef1682892f7a927fa2b0d9ce5daecd6a7b2be4e
shoxrux18 <shohruxturdimov@gmail.com>
2022-03-15 22:37:34 +0500

    ...

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,8 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <MainMenu/>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+import MainMenu from "@/components/MainMenu.vue";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import "@/assets/main.scss"
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App).use(ElementPlus).mount('#app')
